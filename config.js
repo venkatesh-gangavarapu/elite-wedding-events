903bb427a892978a594f4e0c15b4bd933504895a
Venkatesh Gangavarapu <gangavarapuvenkatesh3@gmail.com>
2026-08-16 12:05:02 +0530

    Update DB url

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // These are safe to expose in a browser when Row Level Security is configured correctly.
 // NEVER put a Supabase service_role key in this file.
 window.EWE_CONFIG = {
-  url: "https://gyoganjrpektvhpbcasm.supabase.co/rest/v1/",
+  url: "https://gyoganjrpektvhpbcasm.supabase.co",
   anonKey: "sb_publishable_agwIY3HLHh6BO-fxf-bUaw_IfG8q1eB"
 };
